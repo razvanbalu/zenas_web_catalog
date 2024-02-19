65032e739e068caca8dbd26b6d850c36c956ef80
razvanbalu <60687481+razvanbalu@users.noreply.github.com>
2024-02-19 07:51:19 +0200

    Update zenas_web_catalog.py
    removing parents

--- zenas_web_catalog.py
+++ zenas_web_catalog.py
@@ -1,7 +1,3 @@
-import streamlit
-
-streamlit.title('My Parents New Healthy Dinner')
-
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),CURRENT_REGION()")
